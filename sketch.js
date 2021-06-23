57d95dacb5f9ed2c6b5cb322fc66cf8ec585d83a
Ranvir-k <76581246+Ranvir-k@users.noreply.github.com>
2021-06-23 11:13:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var  database;
+var gameState=0;
+var playerCount;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game();
+  game.getState();
+  game.start();
+}
